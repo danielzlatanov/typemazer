09acbf8fb23531d026066d05b8f3cebc911f6cec
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-21 15:32:55 +0200

    add initial input tracking logic in typing cmp

--- client/src/app/shared/typing/typing.component.ts
+++ client/src/app/shared/typing/typing.component.ts
@@ -9,5 +9,29 @@ export class TypingComponent {
   dummyText: string =
     "This is some dummy text I've typed just now. Type it as fast as possible.";
   words: string[] = this.dummyText.split(/\s+/);
+
+  currentIndex = 0;
   userInput = '';
+
+  onInputChange() {
+    const currentWord = this.words[this.currentIndex] + ' ';
+    const lastWord = this.words[this.words.length - 1];
+
+    if (
+      this.userInput === currentWord ||
+      (currentWord.trim() === lastWord && this.userInput === lastWord)
+    ) {
+      this.moveToNextWord();
+    }
+  }
+
+  moveToNextWord() {
+    if (this.currentIndex < this.words.length - 1) {
+      this.currentIndex++;
+      this.userInput = '';
+    } else {
+      alert('practice completed');
+    }
+  }
+
 }
