09551b31a7ed78083a59b8cf3ed9cdcaa3a7f54b
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-21 14:38:40 +0200

    create 'type' module & 'practice' cmp

---
+++ client/src/app/type/type.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PracticeComponent } from './practice/practice.component';
+
+@NgModule({
+  declarations: [PracticeComponent],
+  imports: [CommonModule],
+})
+export class TypeModule {}
