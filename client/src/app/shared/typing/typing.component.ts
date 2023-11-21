2361b3ddb1a328f4266db97ac2f314032abbbd57
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-21 15:30:59 +0200

    add initial text display in typing cmp

--- client/src/app/shared/typing/typing.component.ts
+++ client/src/app/shared/typing/typing.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./typing.component.css'],
 })
 export class TypingComponent {
+  dummyText: string =
+    "This is some dummy text I've typed just now. Type it as fast as possible.";
+  words: string[] = this.dummyText.split(/\s+/);
+  userInput = '';
 }
