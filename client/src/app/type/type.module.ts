42d48e325cb1cc0562ee3150fe3dfda63b7afb8e
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-21 15:14:48 +0200

    create type routing & add practice route

--- client/src/app/type/type.module.ts
+++ client/src/app/type/type.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TypeRoutingModule } from './type-routing.module';
 import { PracticeComponent } from './practice/practice.component';
 
 @NgModule({
   declarations: [PracticeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TypeRoutingModule],
 })
 export class TypeModule {}
