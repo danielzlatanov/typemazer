c2e340be473bded92814e84992588a231497cb02
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-22 11:22:03 +0200

    implement initial total wpm calculation logic

--- client/src/app/shared/typing/typing.component.ts
+++ client/src/app/shared/typing/typing.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-typing',
   templateUrl: './typing.component.html',
   styleUrls: ['./typing.component.css'],
 })
-export class TypingComponent {
+export class TypingComponent implements OnDestroy {
+  // dummyText: string =
+  //   "This is some dummy text I've typed just now. Type it as fast as possible.";
   dummyText: string =
-    "This is some dummy text I've typed just now. Type it as fast as possible.";
+    "How can I just let you walk away, just let you leave without a trace? When I stand here taking every breath with you, you're the only one who really knew me at all.";
+
   words: string[] = this.dummyText.split(/\s+/);
 
   currentIndex = 0;
   userInput = '';
 
+  startTime!: number;
+  endTime!: number;
+  totalWpm!: number;
   onInputChange() {
+    if (!this.startTime) {
+      this.startTime = Date.now();
+      console.log('race has started:');
+      console.log('start time: ', this.startTime);
+    }
+
     const currentWord = this.words[this.currentIndex] + ' ';
     const lastWord = this.words[this.words.length - 1];
 
@@ -30,7 +42,11 @@ export class TypingComponent {
       this.currentIndex++;
       this.userInput = '';
     } else {
-      alert('practice completed');
+      this.endTime = Date.now();
+      this.calculateTotalWPM();
+
+      console.log('Practice completed:');
+      console.log(`your WPM: ${this.totalWpm}`);
     }
   }
 
@@ -38,5 +54,10 @@ export class TypingComponent {
     if (this.userInput == '') {
       event.preventDefault();
     }
+  }
+
+  calculateTotalWPM() {
+    const minutesElapsed = (this.endTime - this.startTime) / 60000;
+    this.totalWpm = Math.round(this.words.length / minutesElapsed);
   }
 }
