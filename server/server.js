d729680836993c2398bb543a3716c535497243e8
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-26 18:59:27 +0200

    init server structure with initial dependencies & folder layout

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+	res.send('Server is running');
+});
+
+io.on('connection', socket => {
+	console.log('A user connected');
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
