86c677f14dfd8b4422f65a384b1717ac600314e7
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-28 17:09:28 +0200

    add initial create room cmp

---
+++ client/src/app/type/create-room/create-room.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-room',
+  templateUrl: './create-room.component.html',
+  styleUrls: ['./create-room.component.css'],
+})
+export class CreateRoomComponent {
+}
