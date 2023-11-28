264ee7ea878d73766f26ba95d4a7c1b384f93f6e
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-28 17:09:59 +0200

    add create room path to type routing

--- client/src/app/type/type-routing.module.ts
+++ client/src/app/type/type-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PracticeComponent } from './practice/practice.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 const routes: Routes = [
   {
     path: 'practice',
     component: PracticeComponent,
+  },
+  {
+    path: 'create-room',
+    component: CreateRoomComponent,
   },
 ];
 
