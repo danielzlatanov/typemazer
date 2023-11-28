b6c75492c1e754bb2963c3a4913ee9f31b2d3af2
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-28 17:07:55 +0200

    add socket service for client-side handling

---
+++ client/src/app/shared/services/socket.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { io } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  private socket: any;
+
+  constructor() {
+    this.socket = io('http://localhost:8000');
+
+    this.socket.on('connect', () => {
+      console.log('Connected to the server with ID: ', this.socket.id);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from the server');
+    });
+  }
+}
