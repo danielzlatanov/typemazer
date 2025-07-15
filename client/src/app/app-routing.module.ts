23e7a271a83b67d06a84f4a7c429ca365ec698de
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2025-07-15 11:21:17 +0300

    fix home route handling & add wildcard redirect

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
   {
     path: 'type',
     redirectTo: '',
@@ -10,6 +16,10 @@ const routes: Routes = [
   {
     path: 'type',
     loadChildren: () => import('./type/type.module').then((m) => m.TypeModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 
