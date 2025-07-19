3b76bb4779810ac7ec5c9a4eb368718c3413c6a1
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2025-07-19 18:03:16 +0300

    add navbar with dropdown

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavComponent, HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavComponent],
 })
 export class CoreModule {}
