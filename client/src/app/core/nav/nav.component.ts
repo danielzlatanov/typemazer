e8b4919243fa5165353df592dc6f46897fdb1375
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2025-07-19 18:07:32 +0300

    close dropdown on nav & outside click

--- client/src/app/core/nav/nav.component.ts
+++ client/src/app/core/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,4 +6,30 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  isDropdownOpen = false;
+
+  @ViewChild('dropdownBtn') dropdownBtn!: ElementRef;
+  @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    this.isDropdownOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const clickedInsideButton = this.dropdownBtn?.nativeElement.contains(
+      event.target
+    );
+    const clickedInsideMenu = this.dropdownMenu?.nativeElement.contains(
+      event.target
+    );
+
+    if (!clickedInsideButton && !clickedInsideMenu) {
+      this.isDropdownOpen = false;
+    }
+  }
 }
