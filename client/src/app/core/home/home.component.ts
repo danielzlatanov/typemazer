a2c3978630f85c6269a8871955adaf8760252ebd
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2025-07-23 09:32:24 +0300

    adjust main title/desc font size based on screen

--- client/src/app/core/home/home.component.ts
+++ client/src/app/core/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  titleFontSize = '4rem';
+  descFontSize = '1rem';
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.adjustFontSize(event.target.innerWidth);
+  }
+
+  ngOnInit() {
+    this.adjustFontSize(window.innerWidth);
+  }
+
+  private adjustFontSize(width: number) {
+    if (width >= 1280) {
+      this.titleFontSize = '6rem';
+      this.descFontSize = '1.1rem';
+    } else if (width >= 1024) {
+      this.titleFontSize = '5rem';
+      this.descFontSize = '1rem';
+    } else if (width >= 768) {
+      this.titleFontSize = '3.5rem';
+      this.descFontSize = '1.1rem';
+    } else {
+      this.titleFontSize = '3rem';
+      this.descFontSize = '1rem';
+    }
+  }
 }
